783017fe933574ac65c0f3722c4271994e1ea5fc
IsaacReisLindo <isaacreis209@gmail.com>
2025-11-26 10:42:06 -0300

    Inicio De Movimentação e Animação do Felpudo
    Ele mexe e tem animação de andar, falta a animação dele pulando e caindo

---
+++ TFelpudoLove/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D Felpudo;
+    public Transform posicaoDoPe;
+
+    public LayerMask chao;
+
+    private float direcao;
+    public float forcaPulo;
+    public float velocidade;
+    private bool estaNoChao;
+    private bool OlharDireita = true;
+    public Animator animator;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        estaNoChao = Physics2D.OverlapCircle(posicaoDoPe.position, 0.3f, chao);
+
+        direcao = Input.GetAxis("Horizontal");  
+        Felpudo.velocity = new Vector2(direcao * velocidade, Felpudo.velocity.y);
+        animator.SetFloat("velocidade", Mathf.Abs(direcao));
+        if ((direcao < 0 && OlharDireita) || (direcao > 0 && !OlharDireita))
+        {
+           OlharDireita = !OlharDireita;
+            transform.Rotate(0f, 180f, 0f);
+        }
+
+
+        if (estaNoChao && Input.GetButtonDown("Jump"))
+        {
+            Felpudo.AddForce(new Vector2(0, forcaPulo), ForceMode2D.Impulse);
+        }   
+    }
+}
