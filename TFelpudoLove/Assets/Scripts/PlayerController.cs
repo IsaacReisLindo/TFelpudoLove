6f8fbff95c34573aee3df462c4c4b6a7af1c980b
IsaacReisLindo <isaacreis209@gmail.com>
2025-11-29 00:40:44 -0300

    iniciando o dialogo deu tudo errado

--- TFelpudoLove/Assets/Scripts/PlayerController.cs
+++ TFelpudoLove/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D Felpudo;
     public Transform posicaoDoPe;
-
+    DialogueSystem dialogueSystem;
     public LayerMask chao;
 
     private float direcao;
@@ -15,9 +16,20 @@ public class PlayerController : MonoBehaviour
     private bool estaNoChao;
     private bool OlharDireita = true;
     public Animator animator;
+    public Transform npc;
+
+
+
+    private void Awake()
+    {
+        dialogueSystem = FindObjectOfType<DialogueSystem>();
+    }
+
+
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -28,12 +40,12 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("estaNoChao", estaNoChao);
         animator.SetFloat("velocidadeY", Felpudo.velocity.y);
 
-        direcao = Input.GetAxis("Horizontal");  
+        direcao = Input.GetAxis("Horizontal");
         Felpudo.velocity = new Vector2(direcao * velocidade, Felpudo.velocity.y);
         animator.SetFloat("velocidade", Mathf.Abs(direcao));
         if ((direcao < 0 && OlharDireita) || (direcao > 0 && !OlharDireita))
         {
-           OlharDireita = !OlharDireita;
+            OlharDireita = !OlharDireita;
             transform.Rotate(0f, 180f, 0f);
         }
 
@@ -43,6 +55,17 @@ public class PlayerController : MonoBehaviour
         if (estaNoChao && Input.GetButtonDown("Jump"))
         {
             Felpudo.AddForce(new Vector2(0, forcaPulo), ForceMode2D.Impulse);
-        }   
+        }
+
+
+        if (Mathf.Abs(transform.position.x - npc.position.x) < 5.0f)
+        {
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                dialogueSystem.Next();
+            }
+        }
+
     }
 }
---
+++ TFelpudoLove/Assets/Scripts/TypeTextAnimation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TypeTextAnimation : MonoBehaviour
+{
+    public float typeDelay = 0.05f;
+    public TextMeshProUGUI textObject;
+
+    public string fullText;
+    void Start()
+    {
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText ()
+    {
+        textObject.text = fullText;
+        textObject.maxVisibleCharacters = 0;
+        for (int i = 0; i <= textObject.text.Length; i++)
+        {
+            textObject.maxVisibleCharacters = i;
+            yield return new WaitForSeconds(typeDelay);
+        }
+    }
+
+}
